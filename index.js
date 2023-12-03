8f298794018d4396a87c382587df31f370a3c7a8
Charlie Schubach <charlie.schubach@gmail.com>
2023-12-03 12:09:11 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names) {
+    let cardsArr = []
+    for (let i = 0; i < names.length; i++) {
+        cardsArr[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`
+    }
+    return cardsArr
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num)
+        num--
+    }
+}
